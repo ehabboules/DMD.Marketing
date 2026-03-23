eda36f4e338cd08ec99ad626bddd6b9a4859920a
Ehab Abdelmassieh Boules Ibrahim <ehabboules@gmail.com>
2026-03-23 14:18:35 -0400

    Finsh & enahnce the user interface

---
+++ DMD.Marketing/Data/ApplicationUser.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DMD.Marketing.Data;
+
+public class ApplicationUser : IdentityUser { }
--- DMD.Marketing/Program.cs
+++ DMD.Marketing/Program.cs
@@ -1,4 +1,7 @@
+using DMD.Marketing.Data;
 using DMD.Marketing.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +11,57 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped<EmailService>();
+
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseOpenIddict(); // must be called on the same options
+});
+
+// Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// OpenIddict
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+               .UseDbContext<ApplicationDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetAuthorizationEndpointUris("/connect/authorize")
+               .SetTokenEndpointUris("/connect/token")
+               .SetUserinfoEndpointUris("/connect/userinfo")
+               .SetLogoutEndpointUris("/connect/logout");
+
+        options.AllowAuthorizationCodeFlow()
+               .AllowClientCredentialsFlow()
+               .AllowRefreshTokenFlow();
+
+        options.RegisterScopes("openid", "profile", "email", "offline_access");
+
+        // Dev: use ephemeral keys (no cert needed); swap for AddEncryptionCertificate in prod
+        options.AddEphemeralEncryptionKey()
+               .AddEphemeralSigningKey();
+
+        options.UseAspNetCore()
+               .EnableTokenEndpointPassthrough()
+               .EnableAuthorizationEndpointPassthrough()
+               .EnableUserinfoEndpointPassthrough()
+               .EnableLogoutEndpointPassthrough();
+    })
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+        options.UseAspNetCore();
+    });
 
 var app = builder.Build();
 
@@ -19,6 +73,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapRazorComponents<DMD.Marketing.Components.App>()
